4fd8249fd17af61c8eee962ec070b0445f845e2f
waldeyr <waldeyr@gmail.com>
2019-04-28 10:07:07 -0300

    Inicio dos trabalhos na reformulacao do TADS IFG formosa para 2020/2;

---
+++ compilar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pdflatex ppc_tads_2020 -interaction=nonstopmode
+
+biber ppc_tads_2020
+
+pdflatex ppc_tads_2020 -interaction=nonstopmode
+
+pdflatex ppc_tads_2020 -interaction=nonstopmode
+
+rm -f *.aux *.mtc* *.out *.idx *.bcf *.log *~ *.ptc *.toc *.run.xml *.maf *.blg *.bbl
