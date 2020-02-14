451e121cccda5cb50c35639a472b3ea8d4779a12
waldeyr <waldeyr@gmail.com>
2020-02-14 12:15:18 -0300

    Inicio dos trabalhos em 2020;

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Este repositório tem como finalidade guardar a memória dos arquivos da reformu
 
 Pontos a serem discutidos e consolidados:
 
-* Impacto na carga horária dos professores de cada área
 * Bibliografia das ementas
+* Dupla certificação
+* Possibilidade de aproveitamento de FIC para Disciplinas desde compatível em carga horária e ementa
+* Impacto na carga horária dos professores de cada área
+* Adiamento da entrada para segundo semestre de cada ano
 * Revisão geral
+
--- compilar.sh
+++ compilar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -f *.aux *.mtc* *.out *.idx *.bcf *.log *~ *.ptc *.toc *.run.xml *.maf *.blg
+rm -f *.lof *.lot *.brf *.aux *.mtc* *.out *.idx *.bcf *.log *~ *.ptc *.toc *.run.xml *.maf *.blg
 
-pdflatex ppc_tads_2020 -interaction=nonstopmode
+pdflatex tads2020 -interaction=nonstopmode
 
-biber ppc_tads_2020
+bibtex tads2020
 
-pdflatex ppc_tads_2020 -interaction=nonstopmode
+pdflatex tads2020 -interaction=nonstopmode
 
-pdflatex ppc_tads_2020 -interaction=nonstopmode
+pdflatex tads2020 -interaction=nonstopmode
 
-rm -f *.aux *.mtc* *.out *.idx *.bcf *.log *~ *.ptc *.toc *.run.xml *.maf *.blg *.bbl
+rm -f *.lof *.lot *.brf *.aux *.mtc* *.out *.idx *.bcf *.log *~ *.ptc *.toc *.run.xml *.maf *.blg
